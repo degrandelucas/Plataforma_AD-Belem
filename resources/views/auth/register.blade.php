6f9e25f75e299c6ed169a8ff680cde847432d150
degrandelucas <lucasdegrande15@gmail.com>
2025-03-13 17:07:21 -0300

    register.blade.php file was formatted

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -10,17 +10,17 @@
     <label for="name">Nome:</label>
     <input type="text" name="name" id="name" required><br><br>
     @error('name')
-    <div class="alert alert-danger">{{ $message }}</div>
+        <div class="alert alert-danger">{{ $message }}</div>
     @enderror
     <label for="email">Email:</label>
     <input type="email" name="email" id="email" required><br><br>
     @error('email')
-    <div class="alert alert-danger">{{ $message }}</div>
+        <div class="alert alert-danger">{{ $message }}</div>
     @enderror
     <label for="password">Senha:</label>
     <input type="password" name="password" id="password" required><br><br>
     @error('password')
-    <div class="alert alert-danger">{{ $message }}</div>
+        <div class="alert alert-danger">{{ $message }}</div>
     @enderror
     <button type="submit">Registrar</button>
 </form>
