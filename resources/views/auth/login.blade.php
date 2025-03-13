bba50e9a157c017bd960f48b8bfda5bcca512f08
degrandelucas <lucasdegrande15@gmail.com>
2025-03-13 17:01:53 -0300

    Change class login.blade.php, inserting error messages for future use in the view, with @error

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -7,10 +7,28 @@
 <h1>Login</h1>
 <form method="POST" action="/login">
     @csrf
+
+    @if ($errors->any())
+        <div class="alert alert-danger">
+            {{ $errors->first() }}
+        </div>
+    @endif
+
     <label for="email">Email:</label>
-    <input type="email" name="email" id="email" required><br><br>
+    <input type="email" name="email" id="email" required>
+    @error('email')
+    <div class="alert alert-danger">{{ $message }}</div>
+    @enderror
+
     <label for="password">Senha:</label>
-    <input type="password" name="password" id="password" required><br><br>
+    <input type="password" name="password" id="password" required>
+    @error('password')
+    <div class="alert alert-danger">{{ $message }}</div>
+    @enderror
+
+    <input type="checkbox" name="remember" id="remember">
+    <label for="remember">Lembrar-me</label><br><br>
+
     <button type="submit">Entrar</button>
 </form>
 </body>
