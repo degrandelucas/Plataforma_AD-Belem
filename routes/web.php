07c8ade9d3bd1b90b0a425b786943e92541fc5f3
degrandelucas <lucasdegrande15@gmail.com>
2025-03-14 11:06:02 -0300

    created new rout of dashboard after login and created autoload.php with composer

--- routes/web.php
+++ routes/web.php
@@ -7,3 +7,6 @@ Route::get('/login', [AuthController::class, 'showLoginForm'])->name('login');
 Route::post('/login', [AuthController::class, 'login']);
 Route::get('/register', [AuthController::class, 'showRegistrationForm'])->name('register');
 Route::post('/register', [AuthController::class, 'register']);
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware('auth');
